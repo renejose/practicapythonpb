7ba7c2f8394ba417afa240d33d0e23ae4e31f271
phirstone <phirstone@gmail.com>
2025-03-10 21:45:22 -0500

    cambios

---
+++ estructura_repetitiva/ejemplo1.py
@@ -0,0 +1,11 @@
+# Algoritmo Secuencial: Cálculo del área de un rectángulo
+
+# Entrada de datos
+base = float(input("Ingrese la base del rectángulo: "))
+altura = float(input("Ingrese la altura del rectángulo: "))
+
+# Proceso
+area = base * altura
+
+# Salida
+print(f"El área del rectángulo es: {area}")
--- programabucles_miguel.py
+++ programabucles_miguel.py
@@ -1,1 +1,13 @@
 print("hola a todos mi nombre es miguel")
+
+# Algoritmo Secuencial: Cálculo del área de un rectángulo
+
+# Entrada de datos
+base = float(input("Ingrese la base del rectángulo: "))
+altura = float(input("Ingrese la altura del rectángulo: "))
+
+# Proceso
+area = base * altura
+
+# Salida
+print(f"El área del rectángulo es: {area}")
