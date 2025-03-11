de7d23c0d66bb9200f9bcf8b840ab523acd9fde0
renejose <ingenieroshardware@gmail.com>
2025-03-10 21:59:26 -0500

    se me ocurrio cambiar algo

---
+++ estructura_condicional/ejemplo1.py
@@ -0,0 +1,9 @@
+# 1. Solicitar al usuario la base y la altura
+base = float(input("Ingrese la base del rectángulo: "))
+altura = float(input("Ingrese la altura del rectángulo: "))
+
+# 2. Calcular el área
+area = base * altura
+
+# 3. Mostrar el resultado
+print(f"El área del rectángulo es: {area}")
