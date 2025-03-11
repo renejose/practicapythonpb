5a73db0a3b63aa631a086c7d4d75c30dfa2e680c
sakm1 <saulqm2@gmail.com>
2025-03-10 21:04:51 -0500

    primera version

---
+++ programabucles_saul.py
@@ -0,0 +1,7 @@
+print("Hola como estan todos ni nombre es Saul")
+print("Vamos a hacer un programa que imprima los numeros del 1 al 10")
+for i in range(1,11):
+    print(i)
+print("Fin del programa")
+# Autor: Saul Garcia
+# Fecha: 10 de marzo de 2025
